8846b3c30d2469680156bfc57fdb56e734101500
stroherdebora <stroherdebora@gmail.com>
2022-06-15 10:48:20 -0300

    heart icon functional

--- lib/shared/heart.dart
+++ lib/shared/heart.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
 
-class Heart extends StatelessWidget {
+class Heart extends StatefulWidget {
   const Heart({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<Heart> createState() => _HeartState();
+}
+
+bool _heartOn = false;
+
+class _HeartState extends State<Heart> {
+  @override
   Widget build(BuildContext context) {
     return IconButton(
+      onPressed: () async {
+        print(_heartOn);
+        setState(() {
+          _heartOn = !_heartOn;
+        });
+        print(_heartOn);
+      },
       icon: Icon(
-        Icons.favorite,
-        color: Colors.grey[400],
+        _heartOn ? Icons.favorite : Icons.favorite_border,
+        color: Colors.red,
         size: 30,
       ),
-      onPressed: () {},
     );
   }
 }
