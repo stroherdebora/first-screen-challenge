01b35ac28c05c25f7a4d48f2dff3ad77544c4e7e
stroherdebora <stroherdebora@gmail.com>
2022-06-16 12:23:08 -0300

    heart functional

--- lib/shared/heart.dart
+++ lib/shared/heart.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:icon_decoration/icon_decoration.dart';
 
 class Heart extends StatefulWidget {
   const Heart({
@@ -10,27 +9,21 @@ class Heart extends StatefulWidget {
   State<Heart> createState() => _HeartState();
 }
 
-bool _heartOn = false;
-
 class _HeartState extends State<Heart> {
-  bool iconSelected = false;
-  bool isFavourite = false;
+  bool isSelected = false;
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
         IconButton(
-          icon: iconSelected
-              ? Icon(Icons.favorite_border,
-                  size: 30, color: iconSelected ? Colors.grey : Colors.red)
-              : Icon(Icons.favorite,
-                  size: 30, color: isFavourite ? Colors.grey : Colors.red),
+          icon: isSelected
+              ? Icon(Icons.favorite, size: 30, color: Colors.red)
+              : Icon(Icons.favorite_border, size: 30, color: Colors.grey),
           onPressed: () {
             setState(() {
-              print('iconSelected: $iconSelected');
-              isFavourite = !isFavourite;
-              iconSelected = !iconSelected;
+              isSelected = !isSelected;
+              print('isSelected: $isSelected');
             });
           },
         ),
@@ -38,20 +31,3 @@ class _HeartState extends State<Heart> {
     );
   }
 }
-
-
-// IconButton(
-      // color: color,
-      // onPressed: () async {
-        // print(_heartOn);
-        // setState(() {
-          // _heartOn = !_heartOn;
-        // });
-        // print(_heartOn);
-      // },
-      // icon: Icon(
-        // _heartOn ? Icons.favorite : Icons.favorite_border,
-        // color: Colors.red,
-        // size: 30,
-      // ),
-      // );
